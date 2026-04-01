3d0221e6ecef595835cfc2e1b94a3e6848352566
Your Name <you@example.com>
2026-04-01 03:51:12 +0000

    Adapt repository layout and template files

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+import setup_translate
+
+pkg = 'SystemPlugins.VPLEDControl'
+setup(name='enigma2-plugin-systemplugins-vfdcontrol',
+       version='3.0',
+       description='vfd controller',
+       package_dir={pkg: 'VPLEDControl'},
+       packages=[pkg],
+       package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
+       cmdclass=setup_translate.cmdclass,  # for translation
+      )

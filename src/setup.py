aba1f9ae9f6ec226ad6fc1c90ea255afec59cf8c
atvcaptain <openatv@gmail.com>
2026-04-02 21:17:30 +0200

    update ipk name

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 import setup_translate
 
-pkg = 'SystemPlugins.VPLEDControl'
+pkg = 'SystemPlugins.VFDControl'
 setup(name='enigma2-plugin-systemplugins-vfdcontrol',
        version='3.0',
        description='vfd controller',
-       package_dir={pkg: 'VPLEDControl'},
+       package_dir={pkg: 'VFDControl'},
        packages=[pkg],
        package_data={pkg: ['images/*.png', '*.png', '*.xml', 'locale/*/LC_MESSAGES/*.mo']},
        cmdclass=setup_translate.cmdclass,  # for translation
